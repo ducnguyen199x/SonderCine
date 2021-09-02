dcec38d8dce8c9596fad9bc5bfc1554cb455ecb5
Duc Nguyen <duc.nguyenthanh1995@gmail.com>
2021-09-02 11:52:09 +0700

    [ref] remove unused functions in MovieDetailNetworkProvider

--- SonderCine/Shared/Networks/Providers/MovieDetailNetworkProvider.swift
+++ SonderCine/Shared/Networks/Providers/MovieDetailNetworkProvider.swift
@@ -10,20 +10,10 @@ import RxCocoa
 import Alamofire
 
 protocol MovieDetailNetworkProvider {
-    func fetchDetail(id: Int, shouldUseCache: Bool) -> Single<Movie>
-    func fetchCredit(id: Int, shouldUseCache: Bool) -> Single<Credit?>
     func fetchDetailAndCredit(id: Int, shouldUseCache: Bool) -> Single<(Movie, Credit?)>
 }
 
 extension MovieDetailNetworkProvider {
-    func fetchDetail(id: Int) -> Single<Movie> {
-        fetchDetail(id: id, shouldUseCache: true)
-    }
-    
-    func fetchCredit(id: Int) -> Single<Credit?> {
-        fetchCredit(id: id, shouldUseCache: true)
-    }
-    
     func fetchDetailAndCredit(id: Int) -> Single<(Movie, Credit?)> {
         fetchDetailAndCredit(id: id, shouldUseCache: true)
     }
@@ -36,11 +26,11 @@ struct MovieDetailNetworkClient: MovieDetailNetworkProvider {
         self.networkProvider = networkProvider
     }
      
-    func fetchDetail(id: Int, shouldUseCache: Bool) -> Single<Movie> {
+    private func fetchDetail(id: Int, shouldUseCache: Bool) -> Single<Movie> {
         networkProvider.get(api: .movieDetail(id: id), cacheType: shouldUseCache ? .default : .noCache)
     }
     
-    func fetchCredit(id: Int, shouldUseCache: Bool) -> Single<Credit?> {
+    private func fetchCredit(id: Int, shouldUseCache: Bool) -> Single<Credit?> {
         networkProvider.get(api: .credits(id: id), cacheType: shouldUseCache ? .default : .noCache).catchAndReturn(nil)
     }
     
